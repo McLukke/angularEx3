1bd8167769e71523f8a56b08947c2c8935fed03c
Luke McKennan <luke.mckennan@gmail.com>
2015-12-14 12:13:55 +0800

    got basic routing to work / something was wrong with .htaccess before preventing routes from working

---
+++ country-list.php
@@ -0,0 +1,4 @@
+Routing<br />Getting started<br /><br />
+<ul>
+	<li ng-repeat="country in countries">{{country.Name}}</li>
+</ul>
--- index.php
+++ index.php
@@ -1,6 +1,43 @@
-<html>
+<html ng-app="countryApp">
 <?php include('partials/head.html'); ?>
 <body>
-hello world
+<div ng-view>
+	Routing<br />Getting started<br /><br />
+</div>
+<script>
+	var countryApp = angular.module('countryApp', ['ngRoute']);
+
+	countryApp.config(function($routeProvider) {
+		$routeProvider.
+		when('/', {
+			templateUrl: 'country-list.php',
+			controller: 'CountryListCtrl'
+		}).
+		when('/test2', {
+			templateUrl: 'test2.php',
+			controller: 'CountryDetailCtrl'
+		}).
+		when('/:countryName', {
+			templateUrl: 'partials/country-detail.php',
+			controller: 'CountryDetailCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+	});
+
+	countryApp.controller('CountryListCtrl', function($scope, $http) {
+		$http.get('http://www.w3schools.com/angular/customers.php').success(function(data) {
+			$scope.countries = data.records;
+
+			$scope.sortField = "Country";
+			$scope.reverse = true;
+		});
+	});
+
+	countryApp.controller('CountryDetailCtrl', function($scope, $routeParams) {
+		console.log($routeParams);
+	});
+</script>
 </body>
 </html>
